88432db12b48d52aea60b1db3ea314f2b20f0707
Oscar <oscar.shrimpton.personal@gmail.com>
2019-03-24 13:28:30 +0000

    Add LICENSE

--- src/quake3/mod.rs
+++ src/quake3/mod.rs
@@ -1,3 +1,20 @@
+// Copyright (C) 2019 Oscar Shrimpton
+// 
+// This file is part of rust_bsp.
+// 
+// rust_bsp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// rust_bsp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with rust_bsp.  If not, see <http://www.gnu.org/licenses/>.
+
 //! Tools for representing Quake 3 BSP files.
 //! Q3 BSP files start with "IBSP" and have version number 0x2e
 
@@ -8,5 +25,5 @@ use crate::quake3::directory::Header;
 
 /// Represents a parsed BSP file.
 pub struct BSPFile {
-    pub directory: Header
+    pub directory: Header,
 }
